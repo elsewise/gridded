2c0a798cd214fc3e2c66d20e5d3d2f9eebf848d8
elsewise <airywp@qq.com>
2016-08-04 14:50:24 +0800

    just do it

--- src/Gridded/Kit/ColumnBuilder.php
+++ src/Gridded/Kit/ColumnBuilder.php
@@ -9,20 +9,43 @@ namespace Gridded\Kit;
  */
 class ColumnBuilder {
 
+	/** @var array Data Column Model List */
 	protected $_columns = array();
 
+	/**
+	 * Generate a Builder
+	 *
+	 * @return ColumnBuilder
+	 */
+	public static function getInstance() {
+		return new ColumnBuilder();
+	}
+
+	/**
+	 * Add a Existing Column Model
+	 *
+	 * @param Column $column
+	 */
 	public function add(Column $column) {
 		array_push($this->_columns, $column);
 	}
 
-	public function create($name, $configures = NULL) {
+	/**
+	 * Create A Column Model
+	 *
+	 * @param string $column_name
+	 * @param array  $configures
+	 *
+	 * @return Column
+	 */
+	public function create($column_name, $configures = array()) {
 		$column = new Column();
 		if (!empty($configures)) {
 			if (is_array($configures)) {
 				$column->set($configures);
 			}
 		}
-		$column->set("name", $name);
+		$column->set("name", $column_name);
 		$this->add($column);
 		return $column;
 	}
